2d24b6e25bb449b2a4331f9d0ea0e217eaff54fa
Jack-kd <2030343617@qq.com>
2026-06-25 03:45:04 +0800

    Initialize Android application build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,36 @@
 
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.jackkd.automation"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.jackkd.automation"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core:1.15.0")
+}
